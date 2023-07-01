06a5cf55bb0e06fc4f1e00771264a7f326c6054a
2Angel000 <mscmorfy_arcangel13@hotmail.com>
2023-07-01 03:57:54 -0600

    Responsive Movil_1.4

--- src/paginas/Informacion.jsx
+++ src/paginas/Informacion.jsx
@@ -94,73 +94,72 @@ export default function Informacion() {
         </div>
 
         <div className="sm:hidden md:hidden">
-          <div className="rounded overflow-hidden shadow-lg bg-gray-100 general">
-            <img
-              className="w-full rounded-full m-auto"
-              src={process.env.PUBLIC_URL + imgPerfil}
-              alt="Imagen de perfil"
-              style={{ width: "290px", height: "290px", marginTop: "20px" }}
-            />
-            <Flotante />
-            <div className="inline-block py-4 pb-1 text-justify">
-              <div className="font-bold text-xl mb-2">
-                <i
-                  className="fas fa-duotone fa-address-card"
-                  style={{ color: "#5F0B74" }}
-                />
-                <span className="espaciado">
-                  Florencio Martinez Miguel Ángel
-                </span>
-              </div>
-              <p className="text-gray-700 text-base">
-                <i
-                  className="fas fa-solid fa-graduation-cap"
-                  style={{ color: "#1398C8" }}
-                />
-                <span className="espaciado">
-                  Ingeniería en Sistemas Computacionales
-                </span>
-                <span className="espaciado text-green-600 font-medium italic">
-                  2019 - Actualidad
-                </span>
-              </p>
-              <p className="text-gray-700 text-base">
-                <i
-                  className="fas fa-location-dot"
-                  style={{ color: "#C81313" }}
-                />
-                <span className="espaciado">
-                  Chilpancingo, Guerrero, México
-                </span>
-              </p>
-            </div>
 
-            <div className="pb-10 habilidades">
+          
+        <div className="rounded overflow-hidden shadow-sm shadow-md bg-gray-100">
+          <img
+            className="w-full rounded-full m-auto"
+            src={process.env.PUBLIC_URL + imgPerfil}
+            alt="Imagen de perfil"
+            style={{ width: "290px", height: "290px", marginTop: "20px" }}
+          />
+          <Flotante />
+          <div className="p-4 text-center">
+            <div className="font-bold text-xl mb-2">
+              <i
+                className="fas fa-duotone fa-address-card"
+                style={{ color: "#5F0B74" }}
+              />
+              <span className="espaciado">Florencio Martinez Miguel Ángel</span>
+            </div>
+            <div className="text-gray-700 text-base">
+              <i
+                className="fas fa-solid fa-graduation-cap"
+                style={{ color: "#1398C8" }}
+              />
+              <span className="espaciado">
+                Ingeniería en Sistemas Computacionales
+              </span>
+              <span className="espaciado text-green-600 font-medium italic">
+                2019 - Actualidad
+              </span>
+            </div>
+            <div className="text-gray-700 text-base">
+              <i className="fas fa-location-dot" style={{ color: "#C81313" }} />
+              <span className="espaciado">Chilpancingo, Guerrero, México</span>
+            </div>
+          </div>
+
+          <div className="p-4">
+            <div className="grid grid-cols-2 gap-4 sm:grid-cols-1">
               {habilidades.map((habilidad, index) => (
                 <Habilidad key={index} habilidad={habilidad.habilidad} />
               ))}
             </div>
+          </div>
 
-            <div
-              className="px-6 pt-5 mb-8 py-4 h-56 grid grid-cols-4 mt-8 gap-4 content-center
-          informacion"
-            >
-              <div className="inline-block py-4">
+          <div className="p-4">
+            <div className="grid grid-cols-1 grid-rows-3 gap-x-0 gap-y-0">
+              
+              <div className="py-4">
                 <div className="font-bold text-xl mb-2">Contacto</div>
                 <Contact />
               </div>
 
-              <div className="inline-block py-4 m-auto col-span-2 content-center">
+              <div className="py-3">
                 <div className="font-bold text-xl mb-2">Conocimientos</div>
                 <Conocimientos />
               </div>
 
-              <div className="inline-block py-4">
+              <div className="py-3">
                 <div className="font-bold text-xl mb-2">Publicaciones</div>
                 <Otros />
               </div>
             </div>
           </div>
+
+
+        </div>
         </div>
       </div>
     </>
